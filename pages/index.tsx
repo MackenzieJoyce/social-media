f49135a57bfbf45648ba7e6c87261b77f680190c
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 18:14:41 -0500

    Styling: added container around entire page. Pushes content off the edges.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
       <AddPost />
       <ExistingPosts />
-    </>
+    </div>
   )
 }
