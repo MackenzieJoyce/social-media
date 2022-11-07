9a9f907ac58892c85a5385be1551287a23fe0aa5
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 17:50:02 -0500

    Added title for section and a simple form; includes input areas for username, content, and img option.

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -1,6 +1,15 @@
 const AddPost = () => {
     return (
         <>
+            <h2>Add Post</h2>
+            <form>
+                <label htmlFor="username">Username</label>
+                <input type="text" placeholder="Username" />
+                <label htmlFor="content">Content</label>
+                <textarea placeholder="Content" rows="4" cols="50" />
+                <label htmlFor="image">Image</label>
+                <input type="file" />
+            </form>
         </>
     )
 }
