9919c0812fee359ef269a273dc8ae5c8974d9703
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 18:25:31 -0500

    Styling: added form styling around entire form.

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -1,15 +1,17 @@
+import styles from '../styles/Home.module.css'
+
 const AddPost = () => {
     return (
         <>
             <h2>Add Post</h2>
-            <form>
+            <form className={styles.form}>
                 <label htmlFor="username">Username</label>
                 <input type="text" placeholder="Username" />
                 <label htmlFor="content">Content</label>
                 <textarea placeholder="Content" rows="4" cols="50" />
                 <label htmlFor="image">Image</label>
                 <input type="file" />
-                <input type="submit" value="Add Post" />
+                <input type="submit" value="Add Post" className={styles.btn} />
             </form>
         </>
     )
