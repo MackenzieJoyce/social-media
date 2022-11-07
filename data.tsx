6570c25bcc37fcc63e74ec531e5ae83ff58ddef1
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 17:37:28 -0500

    Created a post that is in line with the data template. I wanted to make sure the map was working.

---
+++ data.tsx
@@ -0,0 +1,16 @@
+import { DataTemplate } from "./dataTemplate";
+
+export const data: DataTemplate[] = [
+    {
+        id: 1,
+        username: 'John Doe',
+        content: 'I\'m new here',
+        image: 'N/A'
+    },
+    {
+        id: 2,
+        username: 'Sakura Haruno',
+        content: 'This is so cool',
+        image: 'N/A'
+    },
+];
