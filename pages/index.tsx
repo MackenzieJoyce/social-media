69cf3fcffc5440b69498ee13a12c20779a854afa
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 17:20:05 -0500

    Added Post into pages/index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Post from '../components/Post'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Post />
     </>
   )
 }
