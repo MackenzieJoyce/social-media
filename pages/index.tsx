98e8f8f613c8a0866e795dbe78c6f3e4a4066d40
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 17:47:16 -0500

    Added the AddPost component to pages/index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Post from '../components/Post'
+import AddPost from '../components/AddPost'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <AddPost />
       <Post />
     </>
   )
