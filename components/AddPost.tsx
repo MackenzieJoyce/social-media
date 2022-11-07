7119792e355f00555de531ae7f28d74bd7e96d7c
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 18:28:38 -0500

    Styling: added formGroup classname around the different sections of the form. The Form classname now controls the size of the form.

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -5,13 +5,19 @@ const AddPost = () => {
         <>
             <h2>Add Post</h2>
             <form className={styles.form}>
+                <div className={styles.formGroup}>
                 <label htmlFor="username">Username</label>
-                <input type="text" placeholder="Username" />
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className={styles.formGroup}>
                 <label htmlFor="content">Content</label>
-                <textarea placeholder="Content" rows="4" cols="50" />
+                    <textarea placeholder="Content" rows="4" cols="50" />
+                </div>
+                <div className={styles.formGroup}>
                 <label htmlFor="image">Image</label>
-                <input type="file" />
-                <input type="submit" value="Add Post" className={styles.btn} />
+                    <input type="file" />
+                </div>
+                <input type="submit" value="Add Post" />
             </form>
         </>
     )
