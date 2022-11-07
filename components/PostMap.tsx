c9146256cf831fdbdb80d962273e2c8ed1adddf9
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 18:03:15 -0500

    Created a component for the mapping of the saved posts. Brought in data as a prop from the parent.

---
+++ components/PostMap.tsx
@@ -0,0 +1,15 @@
+const PostMap = ({ data }) => {
+    return (
+        <div>
+            {data.map((post:any) => (
+                <div key={post.id}>
+                    <h4>{post.username}</h4>
+                    <p>{post.content}</p>
+                    {/* <img></img> */}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default PostMap
