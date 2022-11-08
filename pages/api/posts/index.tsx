b16ec88ce3e6907469c3ab8c7b4b1859d30d92a7
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 12:03:57 -0500

    Brought in the dbConnection from utils and the Post model. Set up an async await fxn to start putting in the Get and Post.

---
+++ pages/api/posts/index.tsx
@@ -0,0 +1,8 @@
+import dbConnection from '../../../utils/dbConnection';
+import Post from '../../../models/Post';
+
+dbConnection();
+
+export default async (req:any, res:any) => {
+  const { method } = req;
+};
