9439fd16b6d11f5070903724f8dea0298b24a5fc
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 17:08:20 -0500

    Style - restructure the css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f37c30dda4bcfe57"; }
+/******/ 		__webpack_require__.h = function() { return "b6e17820a0f65eb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
