bd7c7e197e58868a237afcf2a23bb9ca7bbf1132
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 12:26:28 -0500

    Added a fxn for server-side rendering. Might change this later because Next v13 has another way of fetching data within the component... Get the content on the page, then optomize.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,30 +4,37 @@ import Header from '../components/Header';
 import AddPost from '../components/AddPost';
 import styles from '../styles/Home.module.css';
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
-      <div className={styles.container}>
-        <header>
-          <Header />
-        </header>
+    <div className={styles.container}>
+      <header>
+        <Header />
+      </header>
 
-        <main>
-          <AddPost />
-          {/* <ExistingPosts /> */}
-          <section>
-            {posts.map((post: any) => {
-              return (
-                <div key={post.id} className={styles.card}>
-                  <h4>{post.username}</h4>
-                  <p>{post.content}</p>
-                  {/* <img></img> */}
-                </div>
-              );
-            })}
-          </section>
-        </main>
-      </div>
+      <main>
+        <AddPost />
+        {/* <ExistingPosts /> */}
+        <section>
+          {posts.map((post: any) => {
+            return (
+              <div key={post.id} className={styles.card}>
+                <h4>{post.username}</h4>
+                <p>{post.content}</p>
+                {/* <img></img> */}
+              </div>
+            );
+          })}
+        </section>
+      </main>
+    </div>
   );
-}
+};
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/posts');
+  const { data } = await res.json();
+
+  return { posts: data };
+};
 
 export default Home;
