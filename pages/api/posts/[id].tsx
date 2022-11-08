b97a62f08d9f0a6350b5765d8c5d551a642cae28
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 15:44:07 -0500

    Added the routes for GET, PUT, and DELETE for specific ids.

---
+++ pages/api/posts/[id].tsx
@@ -0,0 +1,54 @@
+import dbConnection from '../../../utils/dbConnection';
+import Post from '../../../models/Post';
+
+dbConnection();
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  const {
+    query: { id },
+    method
+  } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const post = await Post.findById(id);
+        if (!post) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: post });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'PUT':
+      try {
+        const post = await Post.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true
+        });
+        if (!post) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: post });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'DELETE':
+      try {
+        const deletedPost = await Post.deleteOne({ _id: id });
+        if (!deletedPost) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+};
