bbc16ca62546e674cc7c618464c176fdad75b5e0
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 12:06:11 -0500

    Added the Get for posts using a switch case and a try catch. Eslint was giving issues about an anonymous fxn so I disabled it for this file.

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -3,6 +3,19 @@ import Post from '../../../models/Post';
 
 dbConnection();
 
+/* eslint-disable import/no-anonymous-default-export */
 export default async (req:any, res:any) => {
   const { method } = req;
+  switch (method) {
+    case 'GET':
+      try {
+        const posts = await Post.find({});
+          res
+              .status(200)
+              .json({ success: true, data: posts });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+  }
 };
