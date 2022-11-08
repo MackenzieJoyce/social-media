1c5699301b48aa214197473066dd09c3f4faf14a
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 13:48:47 -0500

    Added Link for creating a post.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 const Header = () => { 
     return(
         <>
             <h1>Social Media</h1>
+            <Link href="/add-post">Create Post</Link>
         </>
     )
 }
