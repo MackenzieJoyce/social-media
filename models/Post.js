c1e68b3ad0e8723bf8d0e9a06554deafc6018b01
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 11:56:26 -0500

    Created models folder with file for the post schema, and a connecction for mongodb.

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose'); 
+
+const PostSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 3,
+        max: 20,
+    },
+    content: {
+        type: String,
+        required: true,
+        max: 1000,
+    },
+});
+
+
+mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-media',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+module.exports = mongoose.models.Post || mongoose.model('Post', PostSchema);
