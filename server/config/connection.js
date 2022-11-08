1bca9c177fd442bae2aafe75cd7fc8d57bc9d8e2
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-07 22:06:09 -0500

    Make the connection an async await.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-media',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+export async function connect() {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-media',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
-);
+}
 
 module.exports = mongoose.connection;
