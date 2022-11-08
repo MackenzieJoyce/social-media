04e3f1197e5aec6ae4b65ae6f067d7e19a2e2f76
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 16:25:36 -0500

    Created a form, but this time, I'm giving the initial variabel the data from the original post. AT THIS POINT - the info updates via Insomnia.

--- pages/[id]/edit.tsx
+++ pages/[id]/edit.tsx
@@ -1,11 +1,22 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const EditPost = ({ post }) => {
-    const router = useRouter()
+    const [formState, setFormState] = useState({ 
+        username: post.username,
+        content: post.content
+    });
+    const router = useRouter() 
+
     
     return (
         <div>
             <h1>Edit Post</h1>
+            <form>
+                <input type="text" name="username" value={formState.username} />
+                <input type="text" name="content" value={formState.content} />
+                <button type="submit">Finished</button>
+            </form>
         </div>
     )
 }
