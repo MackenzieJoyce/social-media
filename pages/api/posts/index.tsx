b3134e5773055fc899c46f3ce879731ecf27e0fe
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 12:07:16 -0500

    Added the Post for posts in the same way as the Get.

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -4,18 +4,27 @@ import Post from '../../../models/Post';
 dbConnection();
 
 /* eslint-disable import/no-anonymous-default-export */
-export default async (req:any, res:any) => {
+export default async (req: any, res: any) => {
   const { method } = req;
   switch (method) {
     case 'GET':
       try {
         const posts = await Post.find({});
-          res
-              .status(200)
-              .json({ success: true, data: posts });
+        res.status(200).json({ success: true, data: posts });
       } catch (error) {
         res.status(400).json({ success: false });
       }
+      break;
+    case 'POST':
+      try {
+        const post = await Post.create(req.body);
+        res.status(201).json({ success: true, data: post });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
       break;
   }
 };
