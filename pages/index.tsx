76ca3aa95e7866994e005d4925fc417664a949c4
Mackenzie <mackenziejoyce414@gmail.com>
2022-11-08 12:20:23 -0500

    Brought in the mapped posts and commented out the ExistingPosts component. Added a little bit of structure to the html. commented out img tag for now.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import ExistingPosts from '../components/ExistingPosts'
-import AddPost from '../components/AddPost'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Header from '../components/Header';
+// import ExistingPosts from '../components/ExistingPosts';
+import AddPost from '../components/AddPost';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <Header />
-      <AddPost />
-      <ExistingPosts />
-    </div>
-  )
+      <div className={styles.container}>
+        <header>
+          <Header />
+        </header>
+
+        <main>
+          <AddPost />
+          {/* <ExistingPosts /> */}
+          <section>
+            {posts.map((post: any) => {
+              return (
+                <div key={post.id} className={styles.card}>
+                  <h4>{post.username}</h4>
+                  <p>{post.content}</p>
+                  {/* <img></img> */}
+                </div>
+              );
+            })}
+          </section>
+        </main>
+      </div>
+  );
 }
+
+export default Home;
